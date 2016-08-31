1db2f01fa8333e9471b49f390ac3bd9d3cc6e08d
mico <mi-const@yandex.ru>
2016-08-31 10:56:56 +0300

    update

---
+++ MakeGetRequest/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace MakeGetRequest
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_Words = new System.Windows.Forms.ListBox();
+            this.textBox_Output = new System.Windows.Forms.TextBox();
+            this.listBox_Urls = new System.Windows.Forms.ListBox();
+            this.button_Play = new System.Windows.Forms.Button();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.textBox_Word = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox_Words
+            // 
+            this.listBox_Words.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox_Words.FormattingEnabled = true;
+            this.listBox_Words.Location = new System.Drawing.Point(12, 38);
+            this.listBox_Words.Name = "listBox_Words";
+            this.listBox_Words.Size = new System.Drawing.Size(172, 329);
+            this.listBox_Words.Sorted = true;
+            this.listBox_Words.TabIndex = 0;
+            this.listBox_Words.SelectedIndexChanged += new System.EventHandler(this.listBox_Words_SelectedIndexChanged);
+            // 
+            // textBox_Output
+            // 
+            this.textBox_Output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Output.Location = new System.Drawing.Point(191, 12);
+            this.textBox_Output.Multiline = true;
+            this.textBox_Output.Name = "textBox_Output";
+            this.textBox_Output.Size = new System.Drawing.Size(781, 178);
+            this.textBox_Output.TabIndex = 1;
+            // 
+            // listBox_Urls
+            // 
+            this.listBox_Urls.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_Urls.FormattingEnabled = true;
+            this.listBox_Urls.Location = new System.Drawing.Point(191, 195);
+            this.listBox_Urls.Name = "listBox_Urls";
+            this.listBox_Urls.Size = new System.Drawing.Size(781, 147);
+            this.listBox_Urls.Sorted = true;
+            this.listBox_Urls.TabIndex = 2;
+            // 
+            // button_Play
+            // 
+            this.button_Play.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_Play.Location = new System.Drawing.Point(191, 343);
+            this.button_Play.Name = "button_Play";
+            this.button_Play.Size = new System.Drawing.Size(75, 23);
+            this.button_Play.TabIndex = 3;
+            this.button_Play.Text = "&Play";
+            this.button_Play.UseVisualStyleBackColor = true;
+            this.button_Play.Click += new System.EventHandler(this.button_Play_Click);
+            // 
+            // button_Save
+            // 
+            this.button_Save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_Save.Location = new System.Drawing.Point(272, 343);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(75, 23);
+            this.button_Save.TabIndex = 4;
+            this.button_Save.Text = "&Save As";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // textBox_Word
+            // 
+            this.textBox_Word.Location = new System.Drawing.Point(12, 12);
+            this.textBox_Word.Name = "textBox_Word";
+            this.textBox_Word.Size = new System.Drawing.Size(172, 20);
+            this.textBox_Word.TabIndex = 5;
+            this.textBox_Word.TextChanged += new System.EventHandler(this.textBox_Word_TextChanged);
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 380);
+            this.Controls.Add(this.textBox_Word);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.button_Play);
+            this.Controls.Add(this.listBox_Urls);
+            this.Controls.Add(this.textBox_Output);
+            this.Controls.Add(this.listBox_Words);
+            this.Name = "Form_Main";
+            this.Text = "Make A Get Request";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_Words;
+        private System.Windows.Forms.TextBox textBox_Output;
+        private System.Windows.Forms.ListBox listBox_Urls;
+        private System.Windows.Forms.Button button_Play;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.TextBox textBox_Word;
+    }
+}
+
---
+++ MakeGetRequest/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace MakeGetRequest
+{
+    public partial class Form_Main : Form
+    {
+        WMPLib.WindowsMediaPlayer Player;
+
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //string sURL;
+            //sURL = @"http://dictionary.cambridge.org/dictionary/english/apropos";
+
+           // WebRequest wrGETURL;
+            //wrGETURL = WebRequest.Create(sURL);
+
+            //WebProxy myProxy = new WebProxy("myproxy", 80);
+            //myProxy.BypassProxyOnLocal = true;
+
+            //wrGETURL.Proxy = WebProxy.GetDefaultProxy();
+
+            //Stream objStream;
+            //objStream = wrGETURL.GetResponse().GetResponseStream();
+
+           // StreamReader objReader = new StreamReader(objStream);
+
+            //string strResponse = objReader.ReadToEnd();
+
+            //string sLine = "";
+            //int i = 0;
+
+            //while (sLine != null)
+            //{
+            //    i++;
+            //    sLine = objReader.ReadLine();
+            //    if (sLine != null)
+            //        Console.WriteLine("{0}:{1}", i, sLine);
+            //}
+            //Console.ReadLine();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            Player = new WMPLib.WindowsMediaPlayer();
+            Player.PlayStateChange += Player_PlayStateChange;
+            Player.MediaError += Player_MediaError;
+
+            string DB_HOST = "localhost";
+            string DB_NAME = "dictionary";
+            string DB_USER = "test_user";
+            string DB_PASS = "12345670";
+
+            string connStr = string.Format("server={0};user={1};database={2};port=3306;password={3};charset=utf8;",
+                DB_HOST, DB_USER, DB_NAME, DB_PASS);
+
+            // Create a connection object.
+            MySqlConnection conn = new MySqlConnection(connStr);
+            conn.Open();
+
+            string query = "SELECT word FROM wordlist WHERE mark > '2016-08-26 00:00:00' AND user='" + DB_USER + "'";
+
+            MySqlDataReader rdr = (new MySqlCommand(query, conn)).ExecuteReader();
+            while (rdr.Read())
+            {
+                listBox_Words.Items.Add(rdr[0].ToString());
+            }
+            rdr.Close();
+
+            conn.Close();
+        }
+
+        private void Player_MediaError(object pMediaObject)
+        {
+            Log("MediaPlayer", pMediaObject.ToString());
+        }
+
+        private void Player_PlayStateChange(int NewState)
+        {
+            if ((WMPLib.WMPPlayState)NewState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                button_Play.Enabled = true;
+            }
+        }
+
+        private void Log(string subject, string message)
+        {
+            textBox_Output.Text += "[" + subject + "] ";
+            textBox_Output.Text += message + "\r\n";
+        }
+
+        private void listBox_Words_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_Word.Text = listBox_Words.SelectedItem.ToString();
+        }
+
+        private void button_Play_Click(object sender, EventArgs e)
+        {
+            string url = listBox_Urls.SelectedItem.ToString();
+            string fname = Path.GetFileName(url);
+
+            if (!File.Exists(fname))
+            {
+                using (var wc = new WebClient())
+                {
+                    wc.DownloadFile(url, fname);
+                }
+            }
+
+            if (File.Exists(fname) && Path.GetExtension(fname).ToLower() != ".ogg")
+            {
+                Player.URL = fname;
+
+                button_Play.Enabled = false;
+                Player.controls.play();
+            }
+        }
+
+        private void button_Save_Click(object sender, EventArgs e)
+        {
+            string url = listBox_Urls.SelectedItem.ToString();
+            string fname = Path.GetFileName(url);
+            string ext = Path.GetExtension(fname);
+            string word = textBox_Word.Text; //listBox_Words.SelectedItem.ToString();
+
+            if (!File.Exists(fname))
+            {
+                using (var wc = new WebClient())
+                {
+                    wc.DownloadFile(url, fname);
+                }
+            }
+
+            if (File.Exists(fname))
+            {
+                
+                string country = "us";
+                if (url.Contains("uk_pron"))
+                {
+                    country = "gb";
+                }
+
+                saveFileDialog.FileName = word + "." + country + ext;
+                saveFileDialog.Filter = "Files|*" + ext;
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    File.Copy(fname, saveFileDialog.FileName, true);
+                }
+            }
+        }
+
+        private void textBox_Word_TextChanged(object sender, EventArgs e)
+        {
+            string word = textBox_Word.Text;
+            if(word.Length <= 0)
+            {
+                return;
+            }
+
+            textBox_Output.Text = "";
+            listBox_Urls.Items.Clear();
+
+            string url = @"http://dictionary.cambridge.org/dictionary/english/" + word;
+
+            WebRequest wr = WebRequest.Create(url);
+
+            Log("url", url);
+            try
+            {
+                Stream sm = wr.GetResponse().GetResponseStream();
+
+                string response = (new StreamReader(sm)).ReadToEnd();
+
+                string pattern = @"(?<=data\-src\-mp3\=)\""([^\""]*)\""";
+                foreach (Match match in Regex.Matches(response, pattern))
+                {
+                    string s = match.Groups[1].Value;
+                    Log("Match", match.Groups[1].Value);
+
+                    if (!listBox_Urls.Items.Contains(s))
+                    {
+                        listBox_Urls.Items.Add(s);
+                    }
+                }
+
+                pattern = @"(?<=data\-src\-ogg\=)\""([^\""]*)\""";
+                foreach (Match match in Regex.Matches(response, pattern))
+                {
+                    string s = match.Groups[1].Value;
+                    Log("Match", match.Groups[1].Value);
+
+                    if (!listBox_Urls.Items.Contains(s))
+                    {
+                        listBox_Urls.Items.Add(s);
+                    }
+                }
+            }
+            catch (System.Net.WebException ex)
+            {
+                Log("error", ex.Message);
+            }
+        }
+    }
+}
