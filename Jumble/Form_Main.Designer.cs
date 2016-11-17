1dc746c8f3e769cebf1e278dd06066b975fbb68b
mico <mi-const@yandex.ru>
2016-11-17 16:25:51 +0300

    update

---
+++ Jumble/Form_Main.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Jumble
+{
+    partial class Form_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_NextWord = new System.Windows.Forms.Button();
+            this.panel_Puzzle = new System.Windows.Forms.Panel();
+            this.label_Guess = new System.Windows.Forms.Label();
+            this.textBox_Answer = new System.Windows.Forms.TextBox();
+            this.Help = new System.Windows.Forms.Button();
+            this.button_OpenWordFile = new System.Windows.Forms.Button();
+            this.openWordFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button_NextWord
+            // 
+            this.button_NextWord.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_NextWord.Location = new System.Drawing.Point(493, 449);
+            this.button_NextWord.Name = "button_NextWord";
+            this.button_NextWord.Size = new System.Drawing.Size(75, 23);
+            this.button_NextWord.TabIndex = 0;
+            this.button_NextWord.Text = "&Next";
+            this.button_NextWord.UseVisualStyleBackColor = true;
+            this.button_NextWord.Click += new System.EventHandler(this.button_NextWord_Click);
+            // 
+            // panel_Puzzle
+            // 
+            this.panel_Puzzle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_Puzzle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_Puzzle.Location = new System.Drawing.Point(13, 13);
+            this.panel_Puzzle.Name = "panel_Puzzle";
+            this.panel_Puzzle.Size = new System.Drawing.Size(555, 208);
+            this.panel_Puzzle.TabIndex = 1;
+            this.panel_Puzzle.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Puzzle_Paint);
+            this.panel_Puzzle.DoubleClick += new System.EventHandler(this.panel_Puzzle_DoubleClick);
+            this.panel_Puzzle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel_Puzzle_MouseDown);
+            this.panel_Puzzle.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel_Puzzle_MouseMove);
+            this.panel_Puzzle.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel_Puzzle_MouseUp);
+            // 
+            // label_Guess
+            // 
+            this.label_Guess.AutoSize = true;
+            this.label_Guess.Location = new System.Drawing.Point(12, 224);
+            this.label_Guess.Name = "label_Guess";
+            this.label_Guess.Size = new System.Drawing.Size(63, 13);
+            this.label_Guess.TabIndex = 2;
+            this.label_Guess.Text = "GuessLabel";
+            this.label_Guess.TextChanged += new System.EventHandler(this.label_Guess_TextChanged);
+            // 
+            // textBox_Answer
+            // 
+            this.textBox_Answer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Answer.Location = new System.Drawing.Point(13, 240);
+            this.textBox_Answer.Multiline = true;
+            this.textBox_Answer.Name = "textBox_Answer";
+            this.textBox_Answer.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox_Answer.Size = new System.Drawing.Size(555, 203);
+            this.textBox_Answer.TabIndex = 0;
+            // 
+            // Help
+            // 
+            this.Help.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Help.Location = new System.Drawing.Point(412, 449);
+            this.Help.Name = "Help";
+            this.Help.Size = new System.Drawing.Size(75, 23);
+            this.Help.TabIndex = 3;
+            this.Help.Text = "&Help";
+            this.Help.UseVisualStyleBackColor = true;
+            this.Help.Click += new System.EventHandler(this.Help_Click);
+            // 
+            // button_OpenWordFile
+            // 
+            this.button_OpenWordFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_OpenWordFile.Location = new System.Drawing.Point(12, 449);
+            this.button_OpenWordFile.Name = "button_OpenWordFile";
+            this.button_OpenWordFile.Size = new System.Drawing.Size(75, 23);
+            this.button_OpenWordFile.TabIndex = 4;
+            this.button_OpenWordFile.Text = "&Open...";
+            this.button_OpenWordFile.UseVisualStyleBackColor = true;
+            this.button_OpenWordFile.Click += new System.EventHandler(this.button_OpenWordFile_Click);
+            // 
+            // openWordFileDialog
+            // 
+            this.openWordFileDialog.DefaultExt = "txt";
+            this.openWordFileDialog.Filter = "Text files|*.txt|All files|*.*";
+            // 
+            // Form_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 484);
+            this.Controls.Add(this.button_OpenWordFile);
+            this.Controls.Add(this.Help);
+            this.Controls.Add(this.textBox_Answer);
+            this.Controls.Add(this.label_Guess);
+            this.Controls.Add(this.panel_Puzzle);
+            this.Controls.Add(this.button_NextWord);
+            this.Name = "Form_Main";
+            this.Text = "Word Jumble";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_NextWord;
+        private System.Windows.Forms.Panel panel_Puzzle;
+        private System.Windows.Forms.Label label_Guess;
+        private System.Windows.Forms.TextBox textBox_Answer;
+        private System.Windows.Forms.Button Help;
+        private System.Windows.Forms.Button button_OpenWordFile;
+        private System.Windows.Forms.OpenFileDialog openWordFileDialog;
+    }
+}
+
---
+++ Jumble/Form_Main.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Configuration;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+#if DEBUG
+using MySql.Data;
+using MySql.Data.MySqlClient;
+#endif
+
+namespace Jumble
+{
+    public partial class Form_Main : Form
+    {
+        List<string> wordsList = new List<string>();
+        List<string> answersList = new List<string>();
+        Random random = new Random();
+        //List<PuzzleTile> tiles = new List<PuzzleTile>();
+        string puzzle = string.Empty;
+        string answer = string.Empty;
+
+        //bool isMouseDown = false;
+        int mouseX = 0;
+        int mouseY = 0;
+        Control selectedTile = null;
+
+        public Form_Main()
+        {
+            InitializeComponent();
+        }
+
+        public void SaveWordlist(string fname)
+        {
+            using (StreamWriter writer = new StreamWriter(fname))
+            {
+                char[] sep = new char[] { '\r', '\n' };
+                for (int i = 0; i < wordsList.Count; i++)
+                {
+                    writer.WriteLine(wordsList[i]);
+                    foreach(string s in answersList[i].Split(sep, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        writer.WriteLine("\t" + s);
+                    }
+                }
+            }
+        }
+
+        public void LoadWordlist(string fname)
+        {
+            using (StreamReader reader = new StreamReader(fname))
+            {
+                string word = string.Empty;
+                string answer = string.Empty;
+                for(string s; (s = reader.ReadLine()) != null;)
+                {
+                    if (s.StartsWith("\t"))
+                    {
+                        answer += s.Trim() + "\r\n";
+                    }
+                    else
+                    {
+                        if(word.Length > 0)
+                        {
+                            wordsList.Add(word);
+                            answersList.Add(answer);
+                        }
+                        word = s.Trim();
+                        answer = string.Empty;
+                    }
+                }
+                if (word.Length > 0)
+                {
+                    wordsList.Add(word);
+                    answersList.Add(answer);
+                }
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // Read settings.
+            var appSettings = ConfigurationManager.AppSettings;
+#if DEBUG
+            try
+            {
+                string connStr = string.Format(
+                    "server={0};user={1};database={2};port=3306;password={3};charset=utf8;",
+                    Properties.Settings.Default.DB_HOST,
+                    Properties.Settings.Default.DB_USER,
+                    Properties.Settings.Default.DB_NAME,
+                    Properties.Settings.Default.DB_PASS
+                );
+
+                // Create a connection object.
+                MySqlConnection conn = new MySqlConnection(connStr);
+                conn.Open();
+
+                string query = "SELECT word,definition FROM wordlist WHERE user=@user_name";
+                var cmd = new MySqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@user_name", Properties.Settings.Default.DB_USER);
+
+                MySqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    wordsList.Add(rdr.GetString(0));
+                    answersList.Add(rdr.GetString(1));
+                }
+                rdr.Close();
+
+                conn.Close();
+            }
+            catch(MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            SaveWordlist(Properties.Settings.Default.DB_USER + ".txt");
+#endif // DEBUG
+
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            //e.Graphics.DrawString(puzzle, Font, Brushes.Black, 10, 10);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+
+        private void button_NextWord_Click(object sender, EventArgs e)
+        {
+            if (wordsList.Count > 0)
+            {
+                int next = random.Next(0, wordsList.Count);
+                answer = answersList[next];
+                SetWord(wordsList[next]);
+                ShuffleTiles();
+            }
+        }
+
+        void SetWord(string word)
+        {
+            puzzle = word;
+
+            selectedTile = null;
+            panel_Puzzle.Controls.Clear();
+            
+            foreach(char ch in word)
+            {
+                PuzzleTile t = new PuzzleTile("" + ch);
+                t.Size = new Size(32, 32);
+                t.Parent = panel_Puzzle;
+            }
+
+            panel_Puzzle.Invalidate();
+        }
+
+        void ShuffleTiles()
+        {
+            selectedTile = null;
+
+            int cx = panel_Puzzle.ClientSize.Width - 32;
+            int cy = panel_Puzzle.ClientSize.Height - 32;
+
+            foreach(Control t in panel_Puzzle.Controls)
+            {
+                t.Location = new Point(random.Next(cx), random.Next(cy));
+            }
+            for(int i = 0; i < panel_Puzzle.Controls.Count; i++)
+            {
+                panel_Puzzle.Controls[random.Next(panel_Puzzle.Controls.Count)].BringToFront();
+            }
+            panel_Puzzle_OnChange();
+        }
+
+        private void panel_Puzzle_MouseDown(object sender, MouseEventArgs e)
+        {
+            selectedTile = null;
+            Rectangle rc = new Rectangle();
+            foreach (Control t in panel_Puzzle.Controls)
+            {
+                rc.Location = t.Location;
+                rc.Size = t.Size;
+                if (rc.Contains(e.X, e.Y))
+                {
+                    mouseX = e.X;
+                    mouseY = e.Y;
+                    selectedTile = t;
+                    selectedTile.BringToFront();
+                    break;
+                }
+            }
+        }
+
+        private void panel_Puzzle_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(selectedTile != null)
+            {
+                if(Control.ModifierKeys == Keys.Control)
+                {
+                    panel_Puzzle_DoubleClick(sender, e);
+                }
+                else
+                {
+                    Rectangle rc = new Rectangle(0, 0, 32 * puzzle.Length, 32);
+                    int x = selectedTile.Location.X + selectedTile.Size.Width / 2;
+                    int y = selectedTile.Location.Y + selectedTile.Size.Height / 2;
+                    if (rc.Contains(x, y))
+                    {
+                        int n = x / 32;
+                        x = 32 * n;
+                        y = 0;
+
+                        if(panel_Puzzle_GetTileAt(x, y) == null)
+                        {
+                            selectedTile.Location = new Point(x, y);
+                        }
+                    }
+
+                    selectedTile = null;
+                    panel_Puzzle_OnChange();
+                }
+            }
+        }
+
+        private void panel_Puzzle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedTile != null && (e.X != mouseX || e.Y != mouseY))
+            {
+                int x = selectedTile.Location.X + e.X - mouseX;
+                int y = selectedTile.Location.Y + e.Y - mouseY;
+                if(x < 0)
+                {
+                    x = 0;
+                }
+                if (x > panel_Puzzle.Size.Width - 32)
+                {
+                    x = panel_Puzzle.Size.Width - 32;
+                }
+                if (y < 0)
+                {
+                    y = 0;
+                }
+                if (y > panel_Puzzle.Size.Height - 32)
+                {
+                    y = panel_Puzzle.Size.Height - 32;
+                }
+
+                selectedTile.Location = new Point(x, y);
+                mouseX = e.X;
+                mouseY = e.Y;
+            }
+        }
+
+        private void panel_Puzzle_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle rc = new Rectangle(0, 0, 32, 32);
+            for(int i = 0; i < puzzle.Length; i++)
+            {
+                rc.X = i * 32;
+                e.Graphics.DrawRectangle(Pens.Black, rc);
+            }
+        }
+
+        private Control panel_Puzzle_GetTileAt(int x, int y)
+        {
+            foreach(Control c in panel_Puzzle.Controls)
+            {
+                if(c.Location.X == x && c.Location.Y == y)
+                {
+                    return c;
+                }
+            }
+            return null;
+        }
+
+        private void panel_Puzzle_OnChange()
+        {
+            string guess = string.Empty;
+            for(int i = 0; i < puzzle.Length; i++)
+            {
+                Control c = panel_Puzzle_GetTileAt(i * 32, 0);
+                if(c != null)
+                {
+                    guess += c.Text;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            label_Guess.Text = guess;
+        }
+
+        private void label_Guess_TextChanged(object sender, EventArgs e)
+        {
+            if(label_Guess.Text == puzzle)
+            {
+                textBox_Answer.Text = "Yes!\r\n\r\n" + answer;
+            }
+            else
+            {
+                textBox_Answer.Text = string.Empty;
+            }
+        }
+
+        private void Help_Click(object sender, EventArgs e)
+        {
+            textBox_Answer.Text = answer;
+        }
+
+        private void panel_Puzzle_DoubleClick(object sender, EventArgs e)
+        {
+            if (selectedTile != null)
+            {
+                // Get it into a first free cell.
+                for (int i = 0; i < puzzle.Length; i++)
+                {
+                    Control c = panel_Puzzle_GetTileAt(i * 32, 0);
+                    if (c == null)
+                    {
+                        selectedTile.Location = new Point(i * 32, 0);
+                        selectedTile = null;
+                        panel_Puzzle_OnChange();
+                        break;
+                    }
+                }
+                selectedTile = null;
+            }
+        }
+
+        private void button_OpenWordFile_Click(object sender, EventArgs e)
+        {
+            if(openWordFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                wordsList.Clear();
+                answersList.Clear();
+                LoadWordlist(openWordFileDialog.FileName);
+            }
+        }
+    }
+
+    class PuzzleTile : UserControl
+    {
+        public PuzzleTile(string name)
+        {
+            Text = name;
+            Enabled = false; // no keyboard events to the tile
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //base.OnPaint(e);
+            var g = e.Graphics;
+
+            g.Clear(SystemColors.Control);
+
+            var cx = Size.Width;
+            var cy = Size.Height;
+            var wx = SystemInformation.FrameBorderSize.Width;
+            var wy = SystemInformation.FrameBorderSize.Height;
+
+            var pts = new Point[]
+            {
+                new Point(0,  cy), new Point(0, 0),
+                new Point(cx,  0), new Point(cx - wx, wy),
+                new Point(wx, wy), new Point(wx, cy - wy),
+            };
+            g.FillPolygon(SystemBrushes.ControlLightLight, pts);
+
+            pts = new Point[]
+            {
+                new Point(cx, 0), new Point(cx, cy),
+                new Point(0, cy), new Point(wx, cy - wy),
+                new Point(cx - wx, cy - wy), new Point(cx- wx, wy),
+            };
+            g.FillPolygon(SystemBrushes.ControlDark, pts);
+
+            var f = new Font("Arial", 24);
+            var sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+            sf.LineAlignment = StringAlignment.Center;
+
+            g.DrawString(Text, f, SystemBrushes.ControlText, ClientRectangle, sf);
+        }
+    }
+}
